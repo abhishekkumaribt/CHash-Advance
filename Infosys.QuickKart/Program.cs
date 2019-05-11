4a88ef0b9b0ffda5de5727aa85b1aeeb10ec07ba
Abhishek Kumar <abhishekkumaribt@gmail.com>
2019-05-11 23:16:22 +0530

    Started

---
+++ Infosys.QuickKart/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuickKartBusinessLayer;
+
+namespace Infosys.QuickKart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product prodObj1 = new Product("P101", "Table", 1034.00, "Good Quality");
+            Product prodObj2 = new Product("P102", "Chair", 1134.00, "Good Quality");
+            Product prodObj3 = new Product("P103", "Books", 534.00, "Good Quality");
+
+            Product[] productList = new Product[5];
+
+            productList[0] = prodObj1;
+            productList[1] = prodObj2;
+            productList[2] = prodObj3;
+            try
+            {
+                for (int i = 0; i < productList.Length; i++)
+                {
+                    Console.WriteLine("ID = " + productList[i].ProductId +
+                                               "\tName = " + productList[i].ProductName +
+                                               "\tPrice = " + productList[i].Price +
+                                               "\tDescription = " + productList[i].Description);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nThere is some error in the program.");
+            }
+            finally
+            {
+                Console.WriteLine("\nEnd of the product list details.");
+            }
+        }
+    }
+}
---
+++ QuickKartBusinessLayer/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickKartBusinessLayer
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public Product(string productId, string productName, double price, string description)
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.Price = price;
+            this.Description = description;
+        }
+    }
+}
